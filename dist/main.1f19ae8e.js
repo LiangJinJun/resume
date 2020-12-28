8520bd3bcd508fb992156256aedb2c2962f912fd
LiangJinJun <2210258654@qq.com>
2020-12-28 19:04:47 +0800

    1

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -194,7 +194,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51308" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55901" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
